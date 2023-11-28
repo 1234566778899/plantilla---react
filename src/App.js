27ee4b80d66a99f8e7254f86114579bc6eff799a
Carlos Ordaz <carlos.ordaz@acomo.com.pe>
2023-11-28 09:54:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Routes } from 'react-router-dom';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+      {/* <NavbarComponent /> */}
+      <Routes>
+
+        {/* <Route exact path='/' element={<Home />} />
+        <Route path='home' element={<Home />} />
+        <Route path='creation-room' element={<CreationRoom />} />
+         <Route path='sala/:codigo' element={<Sala />} />  */}
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
